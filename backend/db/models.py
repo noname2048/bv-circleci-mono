907c43354d0fd7a46c34096ab8c52af159ac3355
sungwook.choi <sungwook.csw@gmail.com>
2022-09-24 15:47:08 +0900

    feat: add db and models

---
+++ backend/db/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+
+from .database import Base
+
+
+class User(Base):
+    __tablename__ = "core_user"
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String, unique=True, index=True)
+    is_active = Column(Boolean, default=True)
