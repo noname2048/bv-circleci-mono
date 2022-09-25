c39a7034c3149b27989a18a8b462ac633c800074
sungwook.choi <sungwook.csw@gmail.com>
2022-09-25 21:31:38 +0900

    feat: update main

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,12 @@
 from datetime import datetime, timedelta, timezone
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from pydantic import BaseModel, Field
+from sqlalchemy.orm import Session
+from .schemas import User
+from .services import get_users
+from .db.models import get_db
+
 
 app = FastAPI()
 
@@ -13,12 +18,22 @@ class IndexResponse(BaseModel):
 
 
 @app.get("/", status_code=200, tags=["index"], response_model=IndexResponse)
-def index():
+async def index():
     now = datetime.now(kst)
     return IndexResponse(server_time=datetime.now(kst).replace(microsecond=0))
 
 
+@app.get("/users", response_model=list[User])
+async def list_user(db: Session = Depends(get_db)):
+    users = get_users(db)
+    return users
+
+
 if __name__ == "__main__":
+    import os
+    from pathlib import Path
+
+    Path(__file__).resolve().parents[1]
     import uvicorn
 
     uvicorn.run("main:app", reload=True)
