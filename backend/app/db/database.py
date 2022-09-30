cc70b823ac5e21cc7c50fae28ac0874be08cba7f
Sungwook Choi <sungwook.csw@gmail.com>
2022-09-30 23:19:13 +0900

    feat: try change to sync to async db

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,15 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import databases
 
 from app.settings import settings
 
-SQLALCHEMY_DATABASE_URL = settings.db_url
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+database = databases.Database(settings.db_url)
+engine = create_engine(settings.db_url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base
+Base = declarative_base()
 
 
 def get_db():
