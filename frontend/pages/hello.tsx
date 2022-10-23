ae54e743ec0571e42d86872520ffc4d8f45fe8fe
Sungwook Choi <sungwook.csw@gmail.com>
2022-10-23 22:10:51 +0900

    feat: make new page name 'hello'

--- frontend/pages/hello.tsx
+++ frontend/pages/hello.tsx
@@ -1,5 +1,9 @@
 import type { NextPage } from "next"
 
-const Hello: NextPage = () = {
-    return "hello"
+const Hello: NextPage = () => {
+    return (
+        <div>hello</div>
+    )
 }
+
+export default Hello
